f026d5869e519fbfefb985186de4563d41f0fce1
Aurimas Niekis <aurimas.niekis@gmail.com>
2016-05-31 17:31:19 +0200

    Added JWT Token Validator

---
+++ src/JWTValidatorMiddleware.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Thruster\Component\HttpMiddlewares;
+
+use Namshi\JOSE\SimpleJWS;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Thruster\Component\HttpMessage\Response;
+
+/**
+ * Class JWTValidatorMiddleware
+ *
+ * @package Thruster\Component\HttpMiddlewares
+ * @author  Aurimas Niekis <aurimas@niekis.lt>
+ */
+class JWTValidatorMiddleware
+{
+    /**
+     * @var resource
+     */
+    private $publicKey;
+
+    /**
+     * @var string
+     */
+    private $encoder;
+
+    /**
+     * @var ResponseInterface
+     */
+    private $invalidResponse;
+
+    public function __construct($publicKey, string $encoder = 'RS256', ResponseInterface $invalidResponse = null)
+    {
+        $this->publicKey = $publicKey;
+        $this->encoder = $encoder;
+
+        if (null !== $invalidResponse) {
+            $this->invalidResponse = $invalidResponse;
+        } else {
+            $this->invalidResponse = new Response(403);
+        }
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        if (false === $request->hasHeader('Authorization')) {
+            return $next($request, $response);
+        }
+
+        $token = $request->getHeaderLine('Authorization');
+        if (false === strpos($token, 'Bearer ')) {
+            return $this->invalidResponse;
+        }
+
+        $token = substr($token, 7);
+
+        /** @var SimpleJWS $jws */
+        $jws = SimpleJWS::load($token, false);
+
+        if (false === $jws->isValid($this->publicKey, $this->encoder)) {
+            return $this->invalidResponse;
+        }
+
+        return $next($request->withAttribute('jwt', $jws->getPayload()), $response);
+    }
+}
---
+++ src/Tests/JWTValidatorMiddlewareTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Thruster\Component\HttpMiddlewares\Tests;
+
+use Namshi\JOSE\SimpleJWS;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Thruster\Component\HttpMessage\Response;
+use Thruster\Component\HttpMessage\ServerRequest;
+use Thruster\Component\HttpMiddlewares\JWTValidatorMiddleware;
+
+/**
+ * Class JWTValidatorMiddlewareTest
+ *
+ * @package Thruster\Component\HttpMiddlewares\Tests
+ * @author  Aurimas Niekis <aurimas@niekis.lt>
+ */
+class JWTValidatorMiddlewareTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMiddleware()
+    {
+        $expected = [
+            'sub' => '1234567890',
+            'name' => 'John Doe',
+            'admin' => true,
+            'iat' => '1464708611'
+        ];
+
+        $jws  = new SimpleJWS(['alg' => 'RS256']);
+        $jws->setPayload($expected);
+
+        $jws->sign(openssl_pkey_get_private('file://' . __DIR__ . '/Fixtures/private.pem', 'tests'));
+
+        $request = new ServerRequest('GET', '/');
+        $request = $request->withAddedHeader(
+            'Authorization',
+            'Bearer ' . $jws->getTokenString()
+        );
+
+        $response = new Response();
+
+        $modifier = new JWTValidatorMiddleware(
+            openssl_pkey_get_public('file://' . __DIR__ . '/Fixtures/public.pem'),
+            'RS256'
+        );
+
+        $givenResponse = $modifier($request, $response, function ($request, $respone) use ($expected) {
+            $this->assertSame($expected, $request->getAttribute('jwt'));
+
+            return $respone;
+        });
+
+        $this->assertEquals($response, $givenResponse);
+    }
+
+    public function testModifierNotMatched()
+    {
+        $request  = new ServerRequest('GET', '/');
+        $response = new Response(200);
+
+        $callback = function (ServerRequestInterface $request, ResponseInterface $givenResponse) use ($response) {
+            $this->assertEquals($response, $givenResponse);
+        };
+
+        $modifier = new JWTValidatorMiddleware('secret', 'RS256');
+        $modifier($request, $response, $callback);
+    }
+
+    public function testMiddlewareInvalidToken()
+    {
+        $request = new ServerRequest('GET', '/');
+        $request = $request->withAddedHeader(
+            'Authorization',
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikpv' .
+            'aG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxoYZgeFONFh7HgQ'
+        );
+
+        $response = new Response();
+
+        $invalidResponse = new Response(403);
+
+        $callback = function (ServerRequestInterface $req, ResponseInterface $givenResponse) use ($invalidResponse) {
+            $this->assertEquals($invalidResponse, $givenResponse);
+        };
+
+        $modifier = new JWTValidatorMiddleware('', 'RS256', $invalidResponse);
+        $modifier($request, $response, $callback);
+    }
+
+    public function testMiddlewareInvalidHeader()
+    {
+        $request = new ServerRequest('GET', '/');
+        $request = $request->withAddedHeader(
+            'Authorization',
+            'foobar'
+        );
+
+        $response = new Response();
+
+        $invalidResponse = new Response(403);
+
+        $callback = function (ServerRequestInterface $req, ResponseInterface $givenResponse) use ($invalidResponse) {
+            $this->assertEquals($invalidResponse, $givenResponse);
+        };
+
+        $modifier = new JWTValidatorMiddleware('', 'RS256', $invalidResponse);
+        $modifier($request, $response, $callback);
+    }
+}
